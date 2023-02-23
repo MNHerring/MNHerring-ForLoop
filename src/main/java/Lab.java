fab1add42d13cfdd1f990714c068e9338bc7e948
MNHerring <maxherringfuture@gmail.com>
2023-02-23 01:11:44 +0000

    added for loop to print out "i" ass "i" added + 1 not including end value

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -41,8 +41,12 @@ public class Lab {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
-
-//        code here
+        
+        //        code here
+               for (int i = start; i < end; i++){
+                buildingString += i + " ";
+               }
+                   
 
         return buildingString;
     }
